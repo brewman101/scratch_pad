a5f00878b817fabe2a87839d9a5a503f27b174ae
brewman101 <cags.beer@outlook.com>
2024-06-10 14:22:31 +1000

    Solution for next leap year

--- The next leap year.py
+++ The next leap year.py
@@ -1,14 +1,13 @@
 # Source:  https://programming-24.mooc.fi/part-2/4-simple-loops
 # Part of the solution
-year=int(input("Year: "))
-if (year%100==0) and (year%400==0):
-    print(f"The next leap year after {year} is {year+4}")
-elif (year%4==0) and (year%100!=0):
-        print(f"The next leap year after {year} is {year+4}")
-elif (year%4==3):
-    print(f"The next leap year after {year} is {year+1}")
-elif (year%4==2):
-    print(f"The next leap year after {year} is {year+2}")
-elif (year%4==1):
-    print(f"The next leap year after {year} is {year+3}")
+inp_year = int(input("Give year:"))
+year = inp_year
+leap = year % 4 == 0 and year %100 != 0 or year % 100 == 0 and year % 400 ==0
 
+if leap:
+   print(f"The next leap year from {year} is {year + 4}")
+
+while not leap:
+    year += 1
+    leap = year % 4 == 0 and year %100 != 0 or year % 100 == 0 and year % 400 ==0
+print(f"The next leap year from {inp_year} is {year}")

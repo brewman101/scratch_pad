95c2a8e018c534864baedc2e2b9f5779d37643a5
brewman101 <cags.beer@outlook.com>
2024-06-11 20:22:03 +1000

    Indexing cats

--- cats program.py
+++ cats program.py
@@ -4,5 +4,7 @@ sentence=input("Give me a sentence: ")
 sentence=sentence.lower()
 if "cat" in sentence:
     print("You're talking about cats")
+    index_of_cat=sentence.find("cat")
+    print(f"It starts at {index_of_cat}")
 else:
     print("You're note talking about cats")

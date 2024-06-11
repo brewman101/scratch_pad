20f51639f72b24ac203b9ad215ec8eae6a9f8fcf
brewman101 <cags.beer@outlook.com>
2024-06-11 20:10:19 +1000

    Cat program searching for strings.

---
+++ cats program.py
@@ -0,0 +1,8 @@
+# This is looking for a string in the sentence
+
+sentence=input("Give me a sentence: ")
+
+if "cat" in sentence:
+    print("You're talking about cats")
+else:
+    print("You're note talking about cats")

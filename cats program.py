f2da5731526093400056c2745caf4a051dc7ee72
brewman101 <cags.beer@outlook.com>
2024-06-11 20:13:38 +1000

    working around capitalisation problem

--- cats program.py
+++ cats program.py
@@ -1,7 +1,7 @@
 # This is looking for a string in the sentence
 
 sentence=input("Give me a sentence: ")
-
+sentence=sentence.lower()
 if "cat" in sentence:
     print("You're talking about cats")
 else:

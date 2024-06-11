9ce41a2dee1994a900396c78c12bee04990fdeb7
brewman101 <cags.beer@outlook.com>
2024-06-11 19:22:18 +1000

    Fix the code countdown solution

---
+++ Fix the code - Countdown.py
@@ -0,0 +1,8 @@
+# Source: https://programming-24.mooc.fi/part-3/1-loops-with-conditions
+
+print("Are you ready?")
+number = int(input("Please type in a number: "))
+while number != 0:
+    print(number)
+    number-=1
+print("Now!")

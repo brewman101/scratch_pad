9c07983b93bcbbb83c5e22e5595cbf4241b775d3
brewman101 <cags.beer@outlook.com>
2024-06-12 17:45:59 +1000

    Substring part 2 solution.

---
+++ substring part2.py
@@ -0,0 +1,10 @@
+# Write your solution here
+word=str(input("Please type in a string: "))
+number=1
+number2=len(word)
+number3=-1
+while number < number2:
+    print(word[number3:])
+    number=number+1
+    number3=number3-1
+print(word)

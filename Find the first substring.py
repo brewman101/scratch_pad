7004ba2d59cb2537418c1b4387a51b49ec1fba68
brewman101 <cags.beer@outlook.com>
2024-06-12 19:44:14 +1000

    First in the substring solution

--- Find the first substring.py
+++ Find the first substring.py
@@ -5,7 +5,13 @@
 word=str(input("Please type in a word: "))
 character=str(input("Please type in a character: "))
 index = word.find(character)
+length=len(word)
 if index==-1:
     print()
-else:
+elif length-index==1:
+    print(word[index])
+elif length-index==2:
+    print(word[index]+word[index+1])
+elif length-index>=3:
     print(word[index]+word[index+1]+word[index+2])
+

8d824b3b2c47158585c63a559d97ae5e65e52dd9
brewman101 <cags.beer@outlook.com>
2024-06-12 14:30:30 +1000

    step one in sum 2 solution

--- The sum of consecutive numbers version 2.py
+++ The sum of consecutive numbers version 2.py
@@ -1,16 +1,5 @@
 # Source:  https://programming-24.mooc.fi/part-3/1-loops-with-conditions
-
-
-
-number=1
-limit=int(input("Limit: "))
-indicator=1
-result="The consecutive sum: "
-while number<limit:
-    result=(f"{result} + 
-    number=number+(indicator+1)
-    if number<limit:
-        indicator=indicator+1
-
-print(number)
-print(result)
+number1=1
+while number1<5:
+    print(number1)
+    number1=number1+1

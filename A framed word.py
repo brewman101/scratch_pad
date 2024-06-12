aab80410fac8e0eb40cddd47f4545311452cb55e
brewman101 <cags.beer@outlook.com>
2024-06-12 17:43:57 +1000

    Some stuff

--- A framed word.py
+++ A framed word.py
@@ -1,10 +1,18 @@
-word="testing"
-
+# Write your solution here
+word="python"
 #word=str(input("Word: "))
+space=" "
 word2="*"
-length=len(word2)
-length=int((30-length)/2)
-print(word2*30)
-print(word)
-print(length)
-print(word2*30)
+length=len(word)/2
+if length%2==0:
+    length=int(14-length)
+    print(length)
+    print(word2*30)
+    print(f"*{space*length}{word}{space*length} *")
+    print(word2*30)
+else:
+    length=int(14-length)
+    print(length)
+    print(word2*30)
+    print(f"*{space*length}{word}{space*length} *")
+    print(word2*30)

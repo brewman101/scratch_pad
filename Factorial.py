b0f3ae9cefd2e168fe486de0d4adba9e52257c4e
brewman101 <cags.beer@outlook.com>
2024-06-13 15:24:09 +1000

    Factorial still wip

--- Factorial.py
+++ Factorial.py
@@ -2,13 +2,30 @@
 number=100
 result=1
 sum=1
+increment=1
+while True:
+    #number=int(input("Please type in a number: "))
+    if number<=0:
+        break
+    else:
+        while result<number:
+            increment=+1
+            sum=sum*(increment+1)
+            result=result+1
+    print(f"The factorial of the number {number} is {sum}")
+print("Thanks and bye!")
+
+
+
+
 while True:
     number=int(input("Please type in a number: "))
     if number<=0:
         break
     else:
         while result<number:
-            sum=sum*(sum+1)
+            increment=+1
+            sum=sum*(increment+1)
             result=result+1
     print(f"The factorial of the number {number} is {sum}")
 print("Thanks and bye!")

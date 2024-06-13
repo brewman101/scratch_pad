d687d1f8fdac857d6b009e190cb116e06a9abdff
brewman101 <cags.beer@outlook.com>
2024-06-13 10:54:32 +1000

    Find all the substrings abandoned.

--- Find all the substrings.py
+++ Find all the substrings.py
@@ -1,7 +1,17 @@
+# 13.6.2024 - I wasn't able to fix this one.
+# There is an infinite loop
+# Source:  https://programming-24.mooc.fi/part-3/2-working-with-strings
+
+# Write your solution here
 word=str(input("Please type in a word: "))
 character=str(input("Please type in a character: "))
-index=word.find(character)
-while True:
-    if len(word)==0:
-        break
-    word=word[index:index+2]
+index = word.find(character)
+length=len(word)
+
+
+while (index+2)<length:
+    index = word.find(character)
+    print(word[(index):(index+3)])
+    word=word[(index+1):]
+    length=len(word)
+
